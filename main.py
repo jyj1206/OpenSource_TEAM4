7c12593dd07ad674db07c9efde20836125567913
jyj1206 <tij6100@naver.com>
2020-06-08 21:17:34 -0700

    day 3 update

--- main.py
+++ main.py
@@ -8,12 +8,33 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from flask import redirect, url_for
+from elasticsearch import Elasticsearch
+import numpy
+from numpy import dot
+from numpy.linalg import norm
+from nltk import word_tokenize
+import math
 
 app = Flask(__name__)
-url_list=[]
-url_totalword=[]
-lines=[]
+
+es_host="127.0.0.1"
+es_port="9200"
+es=Elasticsearch([{'host':es_host,'port':es_port}],timeout=30)
+e={
+    "url":[],
+    "num_of_word":[],
+    "Processing_time":[]
+}
+
 count=0
+lines=[]
+# status
+url_num=0
+status=0
+success=0
+fail=0
+same=0
+
 def cleanText(line):
     text = re.sub('[©®™¶\”\“{\}\-=+,#/\;?^$:.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', ' ', line)
     return text
@@ -21,12 +42,12 @@ def cleanText(line):
 def url_add(url):
     global count
     wordcount=0
-    for temp in url_list:
+    for temp in e["url"]:
         if temp==url :
             return 0
     try:
         res=requests.get(url)
-        url_list.append(url)
+        e["url"].append(url)
         html=BeautifulSoup(res.content,"html.parser")
         html_body=html.find_all('body')
         for line in html_body:
@@ -45,29 +66,45 @@ def url_add(url):
                      lines[count]=lines[count] + ' ' + word.lower()
                 wordcount+=1
             count+=1
-        url_totalword.append(wordcount)
+        e["num_of_word"].append(wordcount)
         return 1
     except:
         return -1
 
 @app.route('/')
 def url_analysis():
-    return render_template('info.html',url=url_list)
+    global es;
+    res=es.index(index='web',doc_type='word',id=1,body=e)
+    return render_template('info.html',url_num=url_num,status=status,success=success,fail=fail,same=same,url_list=e["url"],url_totalword=e["num_of_word"],count=count)
 
 @app.route('/url',methods=['POST'])
 def url_input():
+    global url_num, status
+    url_num=1
     url=request.form['name']
-    url_add(url)
+    status=url_add(url)
     return redirect(url_for('url_analysis'))
 
 @app.route('/urltext',methods=['POST'])
 def url_input2():
+    global url_num, success, fail, same
+    url_num=0
+    success=0
+    fail=0
+    same=0
     f=request.files['file']
     fp=open(f.filename,'r')
     urls=fp.readlines()
     for url in urls:
         url=url.replace('\n','')
-        url_add(url)
+        temp=url_add(url)
+        if(temp==1):
+            success+=1
+        elif(temp==-1):
+            fail+=1
+        else:
+            same+=1
+        url_num+=1
     return redirect(url_for('url_analysis'))
 
 if __name__ == '__main__':
