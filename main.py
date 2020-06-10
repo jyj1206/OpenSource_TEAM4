c48dce8a98807126873cfdd393a59b8309464968
jyj1206 <tij6100@naver.com>
2020-06-09 23:51:14 -0700

    5th fixed

--- main.py
+++ main.py
@@ -37,6 +37,89 @@ def cleanText(line):
     text = re.sub('[©®™¶\”\“{\}\-=+,#/\;?^$:.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', ' ', line)
     return text
 
+word_d = {}
+sent_list = []
+
+def cosine_similarity(x,c):
+    global count
+    sent_list=[]
+    word_d={}
+    for i in range(0,count):
+        process_new_sentence(x[i]['lines'])
+        x[i]['vector']=make_vector(i)
+    v1=x[c]['vector']
+    for i in range(0,count):
+        if(c==i):
+            continue
+        v2=x[i]['vector']
+        dotpro= numpy.dot(v1,v2)
+        cossimil = dotpro/(norm(v1)*norm(v2))
+        x[i]['cossimil'][i]=cossimill
+
+def tf_idf(x,c):
+    sent_list=[]
+    word_d={}
+    for i in range(0,count):
+        process_new_sentence(x[i]['lines'])
+    idf_d = compute_idf()
+    tf_d = compute_tf(sent_list[c])
+    for word,tfval in tf_d.items():
+        x[c]['tf_idf'][word]=tfval*idf_d[word]
+
+def process_new_sentence(s):
+    sent_list.append(s)
+    tokenized = word_tokenize(s)
+    for word in tokenized:
+        if word not in word_d.keys():
+            word_d[word]=0
+        word_d[word] += 1
+
+#cosine similarity
+def make_vector(i):
+    v = []
+    s = sent_list[i]
+    tokenized = word_tokenize(s)
+    for w in word_d.keys():
+        val = 0
+        for t in tokenized:
+            if t==w:
+                val +=1
+        v.append(val)
+    return v
+
+#tf-idf
+def compute_tf(s):
+    bow = set()
+    wordcount_d = {}
+    tokenized = word_tokenize(s)
+    for tok in tokenized:
+        if tok not in wordcount_d.keys():
+            wordcount_d[tok]=0
+        wordcount_d[tok] += 1
+        bow.add(tok)
+
+    tf_d = {}
+    for word,count in wordcount_d.items():
+        tf_d[word]=count/float(len(bow))
+    return tf_d
+
+def compute_idf():
+    Dval = len(sent_list)
+    bow = set()
+    for i in range(0,len(sent_list)):
+        tokenized = word_tokenize(sent_list[i])
+        for tok in tokenized:
+            bow.add(tok)
+    idf_d = {}
+    for t in bow:
+        cnt = 0
+        for s in sent_list:
+            if t in word_tokenize(s):
+                cnt += 1;
+        idf_d[t]=math.log(Dval/cnt)
+
+    return idf_d
+
 def url_add(url):
     global count
     wordcount=0
@@ -63,7 +146,7 @@ def url_add(url):
                 else:
                      lines[count]=lines[count] + ' ' + word.lower()
                 wordcount+=1
-        e[count]={'url':url,'Total_word':wordcount,'Lines':lines[count],'Process_Time' : 0}
+        e[count]={'url':url,'Total_word':wordcount,'Lines':lines[count],'Process_Time' : 0,'cosimil':{}}
         count+=1
         return 1
     except:
